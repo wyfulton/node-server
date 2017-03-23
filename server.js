d6d623a7c5fe876b72418bae433096b877cabbc4
William Fulton <wyfulton@gmail.com>
2017-03-23 17:46:11 -0600

    added projects page

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -45,6 +45,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
